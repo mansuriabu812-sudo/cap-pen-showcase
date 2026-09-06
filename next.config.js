9c33014d06949eb4eff9fe661d74d95bbb4b47f3
mansuriabu812-sudo <mansuriabu812@gmail.com>
2026-09-06 19:57:55 +0530

    Fix: Properly format next.config.js with readable formatting

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {reactStrictMode:true,swcMinify:true,images:{remotePatterns:[]}};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [],
+  },
+};
+
 module.exports = nextConfig;
